0bb892db272155b71d386aa7dfa3ec49836727cb
redotter84 <arthemy.r@gmail.com>
2019-03-04 15:10:04 +0300

    Template

---
+++ template.cpp
@@ -0,0 +1,56 @@
+(c) 2019, redotter
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unordered_set>
+#include <unordered_map>
+#include <random>
+#define pb push_back
+#define pf push_front
+#define popb pop_back
+#define popf pop_front
+#define all(a) (a).begin(), (a).end()
+#define sz(a) (ll)((a).size())
+#define heap priority_queue
+#define hash_map unordered_map
+#define hash_set unordered_set
+#define ft first
+#define sd second
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endl "\n"
+#define y0 y0998244353
+#define y1 y1998244353
+#define die(cond) if (cond) for (;;) {}
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<ll> vll;
+typedef set<ll> sll;
+typedef map<ll, ll> mll;
+const ll inf = numeric_limits<ll>::max() / 2;
+const ld eps = 1e-9;
+const ld pi = acosl(-1);
+template<typename T> inline bool mineq(T& a, T b) { return (a > b) ? (a = b, 1) : 0; }
+template<typename T> inline bool maxeq(T& a, T b) { return (a < b) ? (a = b, 1) : 0; }
+
+void solve(), read();
+
+signed main() {
+	fast;
+	read();
+  solve();
+	return 0;
+}
+
+void solve() {
+
+}
+
+void read() {
+
+}
